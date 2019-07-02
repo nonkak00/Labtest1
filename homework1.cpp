5736659e5037255c2c7d107ce57f6b6213179d67
nonkak00 <52158551+nonkak00@users.noreply.github.com>
2019-07-02 20:08:52 +0700

    Create homework1.cpp

---
+++ homework1.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std ;
+void main()
+{
+	cout << "Student Code : 6206021610013" << endl ;
+	cout << "Student Name : Natanon Nantajittanon" << endl ;
+	cout << "    Division : Information Technology" << endl ;
+	cout << "        Year : 1" << endl ;
+	cout << "        Room : A" << endl ;
+	cout << "         Sex : Male" << endl ;
+	cout << "         Age : 8000000000000" << endl ;
+	cout << "	   #   " << endl ;
+	cout << "	  # #  " << endl ;
+	cout << "	 #   #" << endl ;
+	cout << "	#     #" << endl ;
+	cout << "	#######" << endl ;
+	cout << "	#     #" << endl ;
+}
